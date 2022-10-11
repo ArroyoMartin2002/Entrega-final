a7bce27170d5f29d9a115918131252b4bd4dd84f
Martin Arroyo <arroyomartin2002@gmail.com>
2022-10-11 18:55:26 -0300

    desafio fetch

--- main.js
+++ main.js
@@ -1,63 +1,56 @@
-class Producto{
-    constructor(id, nombre, precio, cantidad){
-        this.id=id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-}
-
-const producto1 = new Producto (1, "THIS PLACE WILL BECOME YOUR TOMB / CD",3500, 1);
-const producto2 = new Producto (2, "ONE EP EMBROIDERED TEE",7500, 1);
-const producto3 = new Producto (3, "ONE EP BLACK HOODIE",13500, 1);
-const producto4 = new Producto (4, "THE OFFERING STICKER",3000, 1);
-
-const productos = [producto1, producto2, producto3, producto4];
+const productos = "json/productos.json";
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 
-
-
 //creo la card del catalogo
-productos.forEach(producto => {
 
-    const {id, nombre, precio} = producto
+fetch(productos)
+    .then(respuesta => respuesta.json())
+    .then(datos =>{
+        datos.forEach(producto => {
 
-    const divProducto = document.createElement("div");
-    divProducto.classList.add("card","col-xl-3", "col-md-6", "col-sm-12");
-    divProducto.innerHTML = `
-                            <div>
-                                <img src="img/${id}.png" class="img-fluid ">
-                                <div class="card-body">
-                                    <h2 class="card-title"> ${nombre}</h2>
-                                    <p class="card-text"> $${precio} </p>
-                                    <button id="boton${id}" class="btn btn-dark"> Add To Cart </button>
-                                </div>
-                            </div>`;
-    contenedorProductos.appendChild(divProducto);
-    const boton = document.getElementById(`boton${id}`);
-    boton.addEventListener("click", () => {
-        agregarAlCarrito(id);
+            const {id, nombre, precio} = producto
+        
+            const divProducto = document.createElement("div");
+            divProducto.classList.add("card","col-xl-3", "col-md-6", "col-sm-12");
+            divProducto.innerHTML = `
+                                    <div>
+                                        <img src="img/${id}.png" class="img-fluid ">
+                                        <div class="card-body">
+                                            <h2 class="card-title"> ${nombre}</h2>
+                                            <p class="card-text"> $${precio} </p>
+                                            <button id="boton${id}" class="btn btn-dark"> Add To Cart </button>
+                                        </div>
+                                    </div>`;
+            contenedorProductos.appendChild(divProducto);
+            const boton = document.getElementById(`boton${id}`);
+            boton.addEventListener("click", () => {
+                agregarAlCarrito(id);
+            })
+        });
+        const agregarAlCarrito = (id) =>{
+    
+            const producto = datos.find(producto => producto.id === id);
+        
+            const productoEnCarrito = carrito.find(producto => producto.id === id);
+        
+            productoEnCarrito ? productoEnCarrito.cantidad++ : carrito.push(producto); 
+        
+            actualizarCarrito();
+        
+            Toastify( {
+                text: "Added To Cart Successfully",
+                duration: 1000,
+                position: "right",
+                close: true,
+            }).showToast();
+        }
     })
-});
 
 const carrito = JSON.parse(localStorage.getItem('carritoStorage')) || [];
 
-const agregarAlCarrito = (id) =>{
-    const producto = productos.find(producto => producto.id === id);
-    const productoEnCarrito = carrito.find(producto => producto.id === id);
 
-    productoEnCarrito ? productoEnCarrito.cantidad++ : carrito.push(producto); 
 
-    actualizarCarrito();
-
-    Toastify( {
-        text: "Added To Cart Successfully",
-        duration: 1000,
-        position: "right",
-        close: true,
-    }).showToast();
-}
 
 const contenedorCarrito = document.getElementById("contenedorCarrito")
 
